2ca8cdc354242aaaf0613c6ff8c67a2b5213f601
mrRoze <mrrozenvald@yandex.ru>
2021-11-13 17:16:19 +0300

    Условия

--- First PY study.py
+++ First PY study.py
@@ -13,6 +13,7 @@ print (a)
 """
 #new
 #Variables and assignment
+"""
 a = 1
 b = 1.0
 c = True
@@ -23,3 +24,20 @@ print (a, b, c)
 
 a1, b1, c1 = 1, 1.0, True
 print (a1, b1, c1)
+"""
+
+#new
+#Basic language constructs
+
+#Conditions
+a =1
+if a == 1:
+    print (1)
+elif a==2:
+    print (1)
+else:
+    print ("Не 1 и не 2")
+
+d = "Yes" if a==1 else "No"
+print(d)
+
