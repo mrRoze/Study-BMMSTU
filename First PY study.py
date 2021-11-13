d9bcdb026f32ee6da32afc4baadb9078552c015b
mrRoze <mrrozenvald@yandex.ru>
2021-11-13 17:03:15 +0300

    Переменные и присваивание

--- First PY study.py
+++ First PY study.py
@@ -3,10 +3,23 @@
 #bob
 
 #Styding "Introduction to the language PY"
-
+"""
 for i in range(3):
     print (i)
 
 for a in range(3):
     pass
 print (a)
+"""
+#new
+#Variables and assignment
+a = 1
+b = 1.0
+c = True
+print (type(a), type(b), type(c))
+
+a = 1; b = 1.0; c = True
+print (a, b, c)
+
+a1, b1, c1 = 1, 1.0, True
+print (a1, b1, c1)
